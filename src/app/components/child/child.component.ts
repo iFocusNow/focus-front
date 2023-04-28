ea6899e61557cfbacf1e13ab65215299861ed29e
avi-2-avi <avicon95@protonmail.com>
2023-04-28 16:03:12 -0500

    chore: comments for what's to be done in apps-table

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent {
-
+  // Obtener los dispositivos
+  // Pasarlos en el selector
+  // Si se detecta cierta opcion en el selector, filtrar el appDevices y el linkDevices por device_id
+  // Pasar los argumentos de devices y links a hijos
 }
