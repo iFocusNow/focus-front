6c8b84600e9ccfb97f04b521ebd4b2a8675e9dbf
avi-2-avi <avicon95@protonmail.com>
2023-05-02 02:37:02 -0500

    chore: add table template inside stepper, inside the dialog

--- src/app/components/add-app-dialog/add-app-dialog.component.ts
+++ src/app/components/add-app-dialog/add-app-dialog.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { App } from 'src/app/models/app';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-add-app-dialog',
   templateUrl: './add-app-dialog.component.html',
   styleUrls: ['./add-app-dialog.component.scss'],
 })
-export class AddAppDialogComponent {
-  constructor(private dialogRef: MatDialogRef<AddAppDialogComponent>) {}
+export class AddAppDialogComponent implements OnInit {
+  dataSource: App[] = [];
+  displayedColumns: string[] = ['logo', 'name'];
+
+  constructor(
+    private dialogRef: MatDialogRef<AddAppDialogComponent>,
+    private appService: AppService
+  ) {}
+
+  ngOnInit(): void {
+    this.getApps();
+  }
+
+  getApps() {
+    this.appService.getApps().subscribe((response: any) => {
+      this.dataSource = response;
+    });
+  }
 
   close(): void {
     this.dialogRef.close();
