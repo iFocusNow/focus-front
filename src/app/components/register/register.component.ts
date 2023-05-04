0216468133b6f706f6e40615ad51cf304c2ce07e
avi-2-avi <avicon95@protonmail.com>
2023-05-04 01:19:06 -0500

    chore: correct resizing in laptops

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,32 +3,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Parent } from 'src/app/models/parent';
 import { ParentService } from 'src/app/services/parent.service';
-import { MatSnackBar, MatSnackBarConfig  } from '@angular/material/snack-bar'
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   registerForm: FormGroup;
   error: string = '';
   hidePassword = true;
 
-  constructor(private formBuilder: FormBuilder, private parentService: ParentService, private router: Router, private snackBar: MatSnackBar) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private parentService: ParentService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
     this.registerForm = this.formBuilder.group({
       last_name_mother: ['', Validators.required],
       last_name_father: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
-  }
-
-  
+  ngOnInit(): void {}
 
   onSubmit() {
     if (this.registerForm.invalid) {
@@ -41,13 +42,11 @@ export class RegisterComponent implements OnInit {
       last_name_father: this.registerForm.controls['last_name_father'].value,
       email: this.registerForm.controls['email'].value,
       password: this.registerForm.controls['password'].value,
-      photo_url: "",
+      photo_url: '',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
     };
 
-
-   
     this.parentService.registerParent(parent).subscribe(
       (result) => {
         this.router.navigate(['']);
@@ -58,14 +57,9 @@ export class RegisterComponent implements OnInit {
         });
       }
     );
-    
-
-    
   }
 
   togglePassword() {
     this.hidePassword = !this.hidePassword;
   }
-  
-
 }
