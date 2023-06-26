39344c7dae1149005b14cc3ed987c53096d6a8c5
avi-2-avi <avicon95@protonmail.com>
2023-06-26 03:07:03 -0500

    feat: session register with localStorage dictionary

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Parent } from 'src/app/models/parent';
 import { ParentService } from 'src/app/services/parent.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -14,6 +13,7 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   error: string = '';
   hidePassword = true;
+  storage = localStorage;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -36,19 +36,28 @@ export class RegisterComponent implements OnInit {
       return;
     }
 
-    const parent: Parent = {
-      id: "",
+    const parentUserDTO = {
       last_name_mother: this.registerForm.controls['last_name_mother'].value,
       last_name_father: this.registerForm.controls['last_name_father'].value,
       email: this.registerForm.controls['email'].value,
       password: this.registerForm.controls['password'].value,
-      photo_url: '',
-      created_at: new Date(),
-      updated_at: new Date(),
+      // Momentary photo
+      photo_url:
+        'https://avatars.steamstatic.com/1cfdee9bd003f0559d045b6223b321dd36c34958_full.jpg',
     };
 
-    this.parentService.registerParent(parent).subscribe(
+    this.parentService.registerParent(parentUserDTO).subscribe(
       (result) => {
+        if (!result) {
+          this.snackBar.open('El usuario ya existe', 'Cerrar', {
+            duration: 10,
+          });
+          return;
+        }
+
+        this.storage.setItem('email', parentUserDTO.email);
+        this.storage.setItem('password', parentUserDTO.password);
+
         this.router.navigate(['']);
       },
       (error) => {
--- src/app/services/parent.service.ts
+++ src/app/services/parent.service.ts
@@ -18,16 +18,10 @@ export class ParentService {
     );
   }
 
-  registerParent(parent: Parent): Observable<Parent> {
-    return this.http.get<Parent[]>(baseUrl + '/session/register-parent').pipe(
-      map((parents) => {
-        const foundParent = parents.find((p) => p.email === parent.email);
-        if (foundParent) {
-          throw new Error('El correo electrónico ya está en uso');
-        }
-        return parent;
-      }),
-      switchMap((parent) => this.http.post<Parent>(baseUrl, parent))
+  registerParent(parentUserDto: any): Observable<boolean> {
+    return this.http.post<boolean>(
+      baseUrl + '/session/register-parent',
+      parentUserDto
     );
   }
 
